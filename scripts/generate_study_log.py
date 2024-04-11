fc8fa80f1ff40cda52c57f3957e29e7aaa9c0c24
hellomatia <101452054+hellomatia@users.noreply.github.com>
2024-04-11 10:43:49 +0900

    fix: 전날을 기준으로 스터디 기록

--- scripts/generate_study_log.py
+++ scripts/generate_study_log.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from github import Github
 
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
@@ -9,15 +9,18 @@ def generate_study_log():
     g = Github(GITHUB_TOKEN)
     repo = g.get_repo(REPO_NAME)
     
-    # 오늘 날짜로 파일명 설정
-    today = datetime.now().strftime("%Y%m%d")
-    md_file_path = f"studyLog/{today}_study_log.md"
+    # 전날 날짜로 파일명 설정
+    yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
+    md_file_path = f"studyLog/{yesterday}_study_log.md"
     os.makedirs(os.path.dirname(md_file_path), exist_ok=True)
     
-    with open(md_file_path, "a") as md_file:
-        md_file.write(f"# {today} Study Log\n\n")
+    with open(md_file_path, "w") as md_file:
+        # 표의 헤더 작성
+        md_file.write(f"# {yesterday} Study Log\n\n")
+        md_file.write("|이름|날짜|스터디내용|\n")
+        md_file.write("|---|---|---|\n")
         
-        # 최근 커밋들 가져오기
+        # 모든 커밋들 가져오기
         commits = repo.get_commits()
         for commit in commits:
             commit_message = commit.commit.message
@@ -25,9 +28,11 @@ def generate_study_log():
             if " / " in commit_message:
                 try:
                     name, date, study_content = commit_message.split(' / ')
-                    # Markdown 파일에 내용 추가
-                    md_content = f"- **Name**: {name}\n- **Date**: {date}\n- **Study Content**: {study_content}\n\n"
-                    md_file.write(md_content)
+                    # 커밋 메시지의 날짜가 전날인지 확인
+                    if ("20" + date) == yesterday:
+                        # Markdown 파일에 내용 추가 (표 형식)
+                        md_content = f"|{name}|{date}|{study_content}|\n"
+                        md_file.write(md_content)
                 except ValueError:
                     # 커밋 메시지 형식이 잘못된 경우 건너뛰기
                     continue
