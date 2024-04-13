e174451e5bd4891eedc42831c8df761d5992febd
hanbinseo <shb204@gmail.com>
2024-04-13 21:36:45 +0900

    Spring MVC BeanValidation

--- hanbinseo/Spring/MVC2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ hanbinseo/Spring/MVC2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,27 @@
 package hello.itemservice.domain.item;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Range;
+
 import lombok.Data;
 
 @Data
 public class Item {
 
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1_000_000)
     private Integer price;
+    
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
