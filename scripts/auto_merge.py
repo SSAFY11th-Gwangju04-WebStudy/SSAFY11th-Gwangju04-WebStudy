235cca9e4f9121b177c0cc008ba6f7524c92e36d
Picka <65598179+JAEKWANG97@users.noreply.github.com>
2024-04-18 00:38:14 +0900

    Update auto_merge.py

--- scripts/auto_merge.py
+++ scripts/auto_merge.py
@@ -2,11 +2,9 @@ import os
 from datetime import datetime, timedelta
 from github import Github
 
-# GitHub 인증을 위한 토큰 설정
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 REPO_NAME = 'SSAFY11th-Gwangju04-WebStudy/SSAFY11th-Gwangju04-WebStudy'
 
-# GitHub 객체 초기화
 g = Github(GITHUB_TOKEN)
 repo = g.get_repo(REPO_NAME)
 
@@ -16,14 +14,14 @@ def label_and_merge_prs():
     for pr in open_prs:
         if yesterday in pr.title:
             pr.add_to_labels('automerge')
+            pr = repo.get_pull(pr.number)  # PR 객체를 새로고침
             print(f"Added automerge label to PR #{pr.number}")
-            if 'automerge' in [label.name for label in pr.labels]:
+            if 'automerge' in [label.name for label in pr.get_labels()]:  # 라벨을 다시 확인
                 if pr.mergeable:
                     pr.merge(merge_method='merge', commit_title=pr.title)
                     print(f"Merged PR #{pr.number} successfully")
                 else:
                     print(f"PR #{pr.number} is not mergeable due to conflicts or repo rules.")
-
 
 if __name__ == '__main__':
     label_and_merge_prs()
