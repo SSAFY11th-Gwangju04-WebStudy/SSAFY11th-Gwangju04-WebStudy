9e28d418eab632dd717e49e7f6f06dd9ef8e3021
hanbinseo <shb204@gmail.com>
2024-04-24 22:53:12 +0900

    Spring MVC error

---
+++ hanbinseo/Spring/MVC2/exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,19 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+	@Override
+	public void customize(ConfigurableServletWebServerFactory factory) {
+		ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+		ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+		ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+		factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+	}
+}
